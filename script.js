09c2990dabdc9550810f4a29cb87ab2e67d71672
Orion Palmer <97369887+Composer0@users.noreply.github.com>
2022-06-01 11:02:16 -0400

    Uploaded First Draft
    Next step is region filter

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const poke_container = document.getElementById('poke-container')
-const pokemon_count = 251
+const pokemon_count = 151
 const colors = {
     fire: '#FDDFDF',
     water: '#DEF3FD',
@@ -17,19 +17,59 @@ const colors = {
     normal: '#f5f5f5'
 }
 
+const main_types = Object.keys(colors)
+console.log(main_types)
+
 const fetchPokemon = async () => {
-    for(let i = 1; i<=pokemon_count; i++) {
-        await getPokemon()
+    for(let i = 1; i <= pokemon_count; i++) {
+        await getPokemon(i)
     }
 }
 
 const getPokemon = async (id) => {
-    const url = `https://pokemon.co/api/v2/pokemon/${id}`
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
     const res = await fetch(url)
     const data = await res.json()
-    console.log(data)
+    createPokemonCard(data)
+
+}
+
+const createPokemonCard = (pokemon) => {
+    const pokemonEl = document.createElement('div')
+    pokemonEl.classList.add('pokemon')
+
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
+    const id = pokemon.id.toString().padStart(3, '0')
+
+    // map establish that type will now refer to type object's type.name
+    const poke_types = pokemon.types.map(type => type.type.name)
+    // console.log(poke_types)
+    const type = main_types.find(type => poke_types.indexOf(type) > -1)
+    const typeName = type[0].toUpperCase() + type.slice(1)
+
+    const color = colors[type]
+
+    // style draws in CSS property! Be aware that certain descriptors change in presentation when ported over to JavaScript.
+    pokemonEl.style.backgroundColor=color
+    
+    const pokemonInnerHTML = `
+    <div class="img-container">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png" alt="">
+    </div>
+    <div class="info">
+        <span class="number">#${id}</span>
+        <h3 class="name">${name}</h3>
+        <small class="type">Type: <span>${typeName}</span></small>
+    </div>
+    `
+
+// div class="pokemon" becomes the parent for pokemonInnerHTML
+    pokemonEl.innerHTML = pokemonInnerHTML
+
+    // adds the HTML just created into the container element previously established at the beginning of the document.
+    // Additional Note.  Your poke-container needs to already be established within the HTML, otherwise you can not attach the Element that you are creating. In other works, screate small bones and then insert the qualities you are looking for using JavaScript.
+    // Fun note, when using the debugger on browser you will not be able to easily determine if the site is created using JavaScript made HTML or natural HTML.
+    poke_container.appendChild(pokemonEl)
 }
 
 fetchPokemon()
-
-const API = "https://pokeapi.co/api/v2/pokemon/1"
